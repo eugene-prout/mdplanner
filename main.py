2d87dce7a649b5d028840a4d46d8bf8e0049693e
Eugene <eugene.prout1@gmail.com>
2022-04-28 16:43:06 +0100

    chore: added click-shell boilerplate

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-print("hello, world!")
+import click
+from click_shell import shell
+
+@shell(prompt='mdplanner > ', intro='Welcome to MD Planner...')
+def core():
+    pass
+
+@core.command()
+def testcommand():
+    click.echo("testcommand is running")
+
+if __name__ == '__main__':
+    core()
